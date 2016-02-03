7de2e376f19fdc90ccd686d3a427c0a7911d78a4
David Amanshia <phpengine@hotmail.co.uk>
2016-02-03 12:17:32 +0000

    batch file draft

---
+++ Windows/Code/pharaoh-windows.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=C:\Users\vagrant\native-installers\Windows\pharaohtools2.exe
+REM BFCPEICON=
+REM BFCPEICONINDEX=1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=0
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=1
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Pharaoh Tools
+REM BFCPEVERDESC=PHP Automation
+REM BFCPEVERCOMPANY=Pharaoh Tools
+REM BFCPEVERCOPYRIGHT=
+REM BFCPEOPTIONEND
+@ECHO ON
+@echo off
+echo "Our Pharaoh Tools Windows Executable"
+timeout 5
+REM Download Git Latest
+echo "Downloading Git"
+REM Install Git
+echo "Downloading Git"
+REM Remove Git Installer Package
+echo "Downloading Git"
+REM Download PHP Latest
+echo "Downloading PHP Latest"
+REM Install PHP Latest
+echo "Installing PHP Latest"
+REM Remove PHP Latest Installer Package
+echo "Downloading PHP Latest Installer Package"
+REM Download Pharaoh configure
+REM php Ptconfigure\install-silent
+REM php Ptconfigure pharaohtools install -yg
+REM Thank You for installing message
