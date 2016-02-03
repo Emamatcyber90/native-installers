994fcf4145239b921b7e2fdb11ecdb60032a6189
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-02-03 04:24:11 +0000

    for rebuilding intstaller

---
+++ Windows/Code/clone-install.cmd
@@ -0,0 +1,3 @@
+"%PROGRAMFILES(X86)\Git\bin\git.exe" clone https://github.com/PharaohTools/ptconfigure ptconfigure
+php ptconfigure\install-silent
+"%SystemDrive%\PharaohTools\ptconfigure.cmd" pharaohtools install --yes --guess
