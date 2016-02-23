40e7602e3c930a17d19b796ccc2489c1848d35f5
benji0522 <benji0522@gmail.com>
2016-02-23 18:18:04 +0530

    Update PreReq.bat

--- Windows/Code/PreReq.bat
+++ Windows/Code/PreReq.bat
@@ -1,12 +1,11 @@
-
-@echo off
 setlocal EnableDelayedExpansion
 set A=C:\ProgramData\chocolatey;C:\php;C:\PharaohTools;C:\Program Files\git\cmd
 set pathToInsert=%A%
 rem Check if pathToInsert is not already in system path
 if "!path:%pathToInsert%=!" equ "%path%" (
+echo "Adding path"
    setx PATH "%PATH%;%A%"  
-timeout 30
+timeout 5
 )else (
 echo "Path is already added"
 )
